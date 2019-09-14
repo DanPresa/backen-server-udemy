71c0d954ddc0a77b14e1a67f6f83d682c2c814ac
DanPresac <danielpresac@dpsoftwaremx.com>
2019-09-14 09:59:01 -0500

    Se crea modelo de usuario

---
+++ model/usuario.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var usuarioSchema = new Schema({
+    nombre: { type: String, required: [true, 'El nombre es necesario'] },
+    correo: { type: String, unique: true, required: [true, 'El correo es necesario'] },
+    password: { type: String, required: [true, 'La contrasea es necesaria'] },
+    img: { type: String, required: false },
+    role: { type: String, default: 'ROLE_USER', required: false }
+});
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
